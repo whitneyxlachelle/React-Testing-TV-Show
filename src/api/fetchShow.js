f03362001ec972c49effc9f4a786761b9983d363
whitneyxlachelle <whiggins16@yahoo.com>
2020-03-11 18:50:48 -0500

    async call moved

---
+++ src/api/fetchShow.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export const fetchShow = () => {
+    return axios
+    .get(
+        "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+      )
+      .then(res => {
+        console.log(res);
+        return res;
+      })
+      .catch(error => {
+          console.log(error)
+          return error;
+      })
+  };
+
+    
